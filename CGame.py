20da4abca4948616733b0e5a989e0938eb2766ac
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2020-10-15 22:53:14 +0200

    Board array added.

--- CGame.py
+++ CGame.py
@@ -5,15 +5,15 @@ from pyglet import image
 
 class CGame:
     window = None
-
-
-    def __init__(self):
-        a = 1
+    board = None
 
     # Prepare new game.
-    def prepare_game(self, window_width=500, window_height=650):
-
+    def prepare_game(self, window_width=500, window_height=650, tile_size=25):
         self.window = pyglet.window.Window(width=window_width, height=window_height)
+
+        if self.window == False:
+            return False
+
         self.window.set_caption("Tetris")
 
         label = pyglet.text.Label('Tetris',
@@ -22,20 +22,18 @@ class CGame:
                                   x=self.window.width // 2, y=self.window.height // 2,
                                   anchor_x='center', anchor_y='center')
 
-
-
         @self.window.event
         def on_draw():
             self.window.clear()
             label.draw()
 
-            sprite = pyglet.sprite.Sprite(pyglet.image.load("assets/img/tile.png"))
-            sprite.x = 0
-            sprite.y = 0
-            sprite.draw()
+         #   self.sprite.draw()
 
+        # Define game board size and create 2D array.
+        cols = rows = 18
+        self.board = [[0] * cols] * rows
 
-
+        # TODO I will have to change this dynamically because the game is getting faster.
         pyglet.clock.schedule_interval(self.update, 1 / 120.0)
 
     # Run a new game.
