647c5cd7960654f0cd11c623dfbe86c2398a8d16
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2020-11-02 21:53:29 +0100

    Add tile texture simple render.

--- CGame.py
+++ CGame.py
@@ -6,15 +6,22 @@ from pyglet import image
 
 class CGame:
     update_interval = 30
+
+    board_size = (10, 35)
+    cell_size = 50
+
     active_shape = None
+    next_shape = None
+    game_board = []
+    tile_texture = None
+
     window = None
-    board = None
 
     # Prepare new game.
     def prepare_game(self, window_width=500, window_height=650, tile_size=25):
         self.window = pyglet.window.Window(width=window_width, height=window_height)
 
-        if self.window == False:
+        if not self.window:
             return False
 
         self.window.set_caption("Tetris")
@@ -30,17 +37,20 @@ class CGame:
             self.window.clear()
             label.draw()
 
-#            self.sprite.draw()
+            self.tile_texture.draw()
+
+        image_source = pyglet.image.load('assets/img/tile.png')
+        self.tile_texture = pyglet.sprite.Sprite(image_source, x=self.cell_size, y=self.cell_size)
 
         # Define game board size and create 2D array.
-        cols = rows = 18
-        self.board = [[0] * cols] * rows
+        # self.game_board = [[0] * cols] * rows
+        self.game_board = [[0] * self.board_size[0]] * self.board_size[1]
 
         # TODO I will have to change this dynamically because the game is getting faster.
         pyglet.clock.schedule_interval(self.update, 1 / self.update_interval)
 
         # Define first shape
-        self.active_shape = CShape()
+        self.active_shape = CShape((0, 0))
 
     # Run a new game.
     def run(self):
--- CShape.py
+++ CShape.py
@@ -5,10 +5,14 @@ from random import randint
 
 # Rotatable shape in game.
 class CShape:
-    __shapes = [[[False, False, False], [False, True, False], [True, True, True]]]
+    __shapes = [[[False, False, False], [False, True, False], [True, True, True]],
+                [[True, True], [True, True]]]
     shape_layout = None
+    location = (0, 0)
 
-    def __init__(self):
+    def __init__(self, spawn_location=(0, 0)):
+        self.location = spawn_location
+        # We need some random layout for this shape.
         # Deep copy needed because we will rotate the shape.
         self.shape_layout = copy.deepcopy(self.__random_shape())
 
@@ -16,6 +20,7 @@ class CShape:
     Generate random index and return random array which represents the shape.
     @:returns Shape array
     """
+
     @staticmethod
     def __random_shape():
         # Generate random index in shapes list - We want new random shape.
@@ -26,6 +31,7 @@ class CShape:
     """
     Rotate shape by 90 degrees left.
     """
+
     def rotate_shape(self):
         self.shape_layout = numpy.rot90(numpy.array(self.shape_layout, bool), 1, (1, 0))
         return
@@ -33,8 +39,8 @@ class CShape:
     """
     Print shape layout.
     """
+
     def print_shape(self):
         N = len(self.shape_layout[0])
         for i in range(N):
             print(self.shape_layout[i])
-
