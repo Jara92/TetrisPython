57f948cfa25ba6406cf5d8a6a09e942824623b0e
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2020-11-22 23:26:48 +0100

    Add tests.

--- test_tetris.py
+++ test_tetris.py
@@ -3,17 +3,66 @@ import numpy
 
 from CShape import CShape
 from CBoard import CBoard
+from NamedTupples import Coord
 
 
 def test_shape_rotation1():
     shape = CShape()
     shape_layout = copy.deepcopy(shape.layout)
 
-    # Rotate 4 time - the shape should be the same
-    shape.rotate_shape()
-    shape.rotate_shape()
-    shape.rotate_shape()
-    shape.rotate_shape()
+    # Rotate n*4 time - the shape should be the same
+    n = 16
+    for i in range(n):
+        shape.rotate_shape()
+        shape.rotate_shape()
+        shape.rotate_shape()
+        shape.rotate_shape()
 
     compare = numpy.array(shape_layout) == numpy.array(shape.layout)
-    assert(compare.all())
+    assert (compare.all())
+
+
+def test_shape_rotation2():
+    shape = CShape()
+    shape.layout = [[False, False, True], [False, True, True], [False, False, True]]
+
+    ref_rotation = [[True, True, True], [False, True, False], [False, False, False]]
+
+    # Rotate shape
+    shape.rotate_shape()
+
+    compare = numpy.array(shape.layout) == numpy.array(ref_rotation)
+    assert (compare.all())
+
+
+def test_shape_movement1():
+    shape = CShape(0, Coord(0, 0))
+    board = CBoard(Coord(20, 30))
+
+    ref_location = (2, 3)
+
+    # move right 2 times
+    shape.move_right(board)
+    shape.move_right(board)
+
+    # move down 3 times
+    shape.move_down(board)
+    shape.move_down(board)
+    shape.move_down(board)
+
+    assert (shape.location == ref_location)
+
+
+def test_shape_movement2():
+    shape = CShape(0, Coord(0, 0))
+    shape.layout = [[True, True], [True, True]]
+    board = CBoard(Coord(20, 30))
+
+    # 29 is last valid y position but shape has "height" tiles 2
+    ref_location = (Coord(0, 28))
+
+    # Trying to move out of board.
+    for i in range(30):
+        shape.move_down(board)
+
+    assert (shape.location == ref_location)
