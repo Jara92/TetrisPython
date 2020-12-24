9962ebc3032db16182ccd5a75c907c5e1860b93f
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2020-12-24 16:32:14 +0100

    Build .exe and edit .gitignore.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from CApplication import CApplication
+import pygame
+import pygame_menu
 
 
 def main():
     # Run application staff.
+    import sys
+    sys.setrecursionlimit(10000)
+
     app = CApplication()
     app.run()
 
